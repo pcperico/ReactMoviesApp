109162988ba9de60f8a97ffdc84846c4044b3f8b
Arturo Salinas <prico.team@gmail.com>
2022-03-03 18:02:51 -0600

    installing formik, creating form for create gender using formik

--- src/Components/Genders/CreateGender.tsx
+++ src/Components/Genders/CreateGender.tsx
@@ -1,7 +1,29 @@
+import { Field, Formik, Form} from "formik";
+import { Link } from "react-router-dom";
+import Button from "../Button";
+
 export default function CreateGender(){
     return (
         <>
             <h1>Create Gender</h1>
+            <Formik initialValues={{
+                name:''
+            }}
+            onSubmit={values=>{
+                console.log(values);
+            }}
+            >
+                <Form>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <Field name="name" className="form-control"/>
+                    </div>
+                    <br />
+                    <Button type="submit" >Save</Button>
+                    <Link className="btn btn-secondary" to="/genders" >Cancel</Link>
+                    
+                </Form>
+            </Formik>
         </>
     )
 }
