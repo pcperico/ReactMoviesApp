2c121902223728e45acc39eaa40a30dd4fe15e4e
Arturo Salinas <prico.team@gmail.com>
2022-03-16 19:24:28 -0600

    adding Create genders consuming external .net core API

--- src/Components/Genders/CreateGender.tsx
+++ src/Components/Genders/CreateGender.tsx
@@ -1,15 +1,27 @@
+import axios from "axios";
+import { createGenderDto } from "./genders.model";
 import GendersForm from "./GendersForm";
-
+import {urlGenders} from "../../endpoints";
+import { useHistory } from "react-router-dom";
 
 export default function CreateGender(){
+    const history = useHistory();
+    async function create(gender:createGenderDto) {
+        try{
+            await axios.post(urlGenders,gender);
+            history.push('/genders');
+        } 
+        catch(error)
+        {
+            console.error(error);
+        }   
+    }
     return (
         <>
             <GendersForm title="Create Gender" model={{name:'',description:''}} 
             onSubmit={async values=>{
                                         console.log("Creating....")
-                                        await new Promise(r=>setTimeout(r,1000));
-                                        console.log(values);
-                                        
+                                        await create(values);
                                     }}  
                
             />
